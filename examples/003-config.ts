33a844ce9245bd29fc5e6875717b444f63ee9e97
mads hargreave <mads.hargreave@gmail.com>
2021-06-08 13:40:01 +0200

    released

---
+++ examples/003-config.ts
@@ -0,0 +1,27 @@
+/**
+ * @file Send a HTTP request
+ */
+
+// file ./krans.config.js
+import { http } from '@krans/http'
+
+export default {
+    input: 'src/main',
+    output: [
+        {
+            file: 'output.json',
+        },
+    ],
+    plugins: [http()],
+}
+
+// file ./main.js
+import { fetch } from '@krans/fetch'
+import { Json } from '@krans/json'
+
+export const main = fetch(Json, 'https://google.com', ({ data }) => ({
+    title: data['title'].toString(),
+    name: data['name'].toString(),
+    description: data['description'].toString(),
+    summary: data['summary'].toString(),
+}))

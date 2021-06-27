aea604ba6b15248d456167a1095f1b652e537d56
mads hargreave <mads.hargreave@gmail.com>
2021-06-27 14:07:00 +0200

    released

--- packages/brod-main/lib/index.ts
+++ packages/brod-main/lib/index.ts
@@ -1,11 +1,1 @@
-export interface FetchResponse {
-    status: number
-    data: any
-}
-
-export async function fetch<T>(
-    url: string,
-    callback: (response: FetchResponse) => T
-): Promise<T> {
-    return callback({ status: 200, data: {} })
-}
+export { run } from '@oclif/command'
